e7ac1e397f0136f37baf08fbd8e1fe2862c08ba7
Al <alexunder_5542@mail.ru>
2021-05-09 10:28:01 +0300

    added ask proj

---
+++ Ask The Circle/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Ask The Circle
+//
+//  Created by tambanco 🥳 on 09.05.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
