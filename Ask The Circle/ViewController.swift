9b41d98302ecfa095a07ac5eae0b450462ef21c6
Al <alexunder_5542@mail.ru>
2021-05-09 11:32:32 +0300

    added 5 palettes

--- Ask The Circle/ViewController.swift
+++ Ask The Circle/ViewController.swift
@@ -10,15 +10,14 @@ import UIKit
 class ViewController: UIViewController
 {
     // MARK: - Properties
+    var palette = Palettes().bluePalette
     var gradientLayer: CAGradientLayer!
     {
         didSet
         {
             gradientLayer.startPoint = CGPoint(x: 0, y: 0)
             gradientLayer.endPoint = CGPoint(x: 1, y: 1)
-            let startColor = #colorLiteral(red: 0.2588235438, green: 0.7568627596, blue: 0.9686274529, alpha: 1).cgColor
-            let endColor = #colorLiteral(red: 0.1764705926, green: 0.4980392158, blue: 0.7568627596, alpha: 1).cgColor
-            gradientLayer.colors = [startColor, endColor]
+            gradientLayer.colors = [palette[0], palette[1], palette[2], palette[3]]
 //            gradientLayer.locations = [0.4, 0.6]
         }
     }
