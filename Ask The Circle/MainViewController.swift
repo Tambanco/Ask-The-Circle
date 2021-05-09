3f341d54236ce36be7ca30fc6ee95ccaa6369909
Al <alexunder_5542@mail.ru>
2021-05-09 13:27:55 +0300

    added PulseAnimation class

--- Ask The Circle/MainViewController.swift
+++ Ask The Circle/MainViewController.swift
@@ -10,7 +10,8 @@ import UIKit
 class MainViewController: UIViewController
 {
     // MARK: - Properties
-    var palette = Palettes().darkBluePalette
+    var palette = Palettes().bluePalette
+    
     var gradientLayer: CAGradientLayer!
     {
         didSet
@@ -18,14 +19,34 @@ class MainViewController: UIViewController
             gradientLayer.startPoint = CGPoint(x: 0, y: 0)
             gradientLayer.endPoint = CGPoint(x: 1, y: 1)
             gradientLayer.colors = [palette[0], palette[1], palette[2], palette[3]]
-//            gradientLayer.locations = [0.4, 0.6]
         }
     }
-
+    
+    // MARK: - Outlets
+    @IBOutlet weak var askButton: UIButton!
+    {
+        didSet
+        {
+            askButton.layer.backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+            askButton.layer.shadowOffset = CGSize(width: 5, height: 5)
+            askButton.layer.shadowOpacity = 0.5
+            askButton.layer.shadowRadius = 10
+        }
+    }
+    
     // MARK: - Life cycle
-    override func viewWillLayoutSubviews()
+    override func viewDidLayoutSubviews()
     {
         gradientLayer.frame = CGRect(x: 0, y: 0, width: self.view.bounds.size.width, height: self.view.bounds.size.height)
+        askButton.frame = CGRect(x: self.view.bounds.size.width / 2 - 100, y: self.view.bounds.size.height / 2, width: 200, height: 200)
+        askButton.layer.cornerRadius = askButton.frame.size.height / 2
+        
+        let path = UIBezierPath()
+        path.move(to: CGPoint(x: self.view.frame.width / 2, y: self.view.frame.height / 2))
+        path.addArc(withCenter: CGPoint(x: self.view.frame.width / 2, y: self.view.frame.height / 2 + 100),
+                    radius: 100.0, startAngle: 4.71239,
+                    endAngle: 10.99558,
+                    clockwise: true)
     }
     
     override func viewDidLoad()
@@ -34,5 +55,19 @@ class MainViewController: UIViewController
         gradientLayer = CAGradientLayer()
         view.layer.insertSublayer(gradientLayer, at: 0)
     }
+    
+    // MARK: - Button actions
+    @IBAction func askButtonPressed(_ sender: UIButton)
+    {
+        let pulse = PulseAnimation(numberOfPulse: Float.infinity, radius: 100, postion: sender.center)
+        pulse.animationDuration = 1.0
+        pulse.backgroundColor = palette[2]
+        self.view.layer.insertSublayer(pulse, below: self.view.layer)
+        let pulse1 = PulseAnimation(numberOfPulse: 15, radius: 200, postion: sender.center)
+        pulse1.animationDuration = 1.4
+        pulse1.backgroundColor = palette[1]
+        self.view.layer.insertSublayer(pulse1, below: self.view.layer)
+    }
+    
 }
 
