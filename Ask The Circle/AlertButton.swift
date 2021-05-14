c81ddb79d1a106c79879a5080c067418a391e4fd
Al <alexunder_5542@mail.ru>
2021-05-14 18:16:51 +0300

    added class AlertButton: UIButton

---
+++ Ask The Circle/AlertButton.swift
@@ -0,0 +1,18 @@
+//
+//  AlertButton.swift
+//  Ask The Circle
+//
+//  Created by tambanco 🥳 on 14.05.2021.
+//
+
+import UIKit
+
+class AlertButton: UIButton
+{
+    override func layoutSubviews()
+    {
+        super.layoutSubviews()
+        
+        self.layer.cornerRadius = 22
+    }
+}
